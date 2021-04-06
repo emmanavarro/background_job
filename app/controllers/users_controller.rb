cf04b2272059eb28ce6dfc46d4722149c73a3ef3
Emma Navarro <elnavarro55@gmail.com>
2021-04-06 10:32:31 -0500

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  require 'faker'
+
+  def index
+    @users = User.all
+  end
+
+  def create_random
+    GenerateRandomUserJob.perform_later
+    redirect_to root_path
+  end
+
+  def destroy_all
+    User.all.destroy_all
+    redirect_to root_path
+  end
+end
